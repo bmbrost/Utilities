a7732e2ead9217100d5538d91a9ebf78399ba16a
Brian M. Brost <brian.brost@noaa.gov>
2017-05-09 14:22:34 -0700

    Minor updates

--- violin.R
+++ violin.R
@@ -4,16 +4,16 @@
 # z <- matrix(rnorm(N*3),N)
 # violin(z,ylab="This is only a test",xlab=c("Violin 1", "Violin 2", "Violin 3"))
 
-violin <- function(z,xlab=NULL,ylab=NULL,ylim=NULL,yat=NULL,bw="SJ",width=0.35,violin.col="gray75",
-	x.buff=0.5,y.buff=0){
+violin <- function(z,xlab=NULL,ylab=NULL,ylim=NULL,yat=NULL,cex.axis=1,padj=0,
+	bw="SJ",smooth=1,violin.width=0.35,violin.col="gray75",x.buff=0.5,y.buff=0){
 
 	# browser()
 	
 	# Create violins
 	n <- ncol(z)  # number of violins
-	den <- apply(z,2,function(x) density(x,bw=bw))  # kernel density
+	den <- apply(z,2,function(x) density(x,bw=bw,adjust=smooth))  # kernel density
 	poly <- lapply(den,function(x)  # rotate and close polygon to create violin
-		cbind(c(x$y,0-rev(x$y))/max(x$y)*width,c(x$x,rev(x$x))))
+		cbind(c(x$y,0-rev(x$y))/max(x$y)*violin.width,c(x$x,rev(x$x))))
 	poly <- sapply(1:n,function(x)  # adjust x-axis values
 		cbind(poly[[x]][,1]+x,poly[[x]][,2]),simplify=FALSE)
 
@@ -29,9 +29,9 @@ violin <- function(z,xlab=NULL,ylab=NULL,ylim=NULL,yat=NULL,bw="SJ",width=0.35,v
 	}
 	if(!is.null(yat)){  # use user-specified y-axis tick marks and lables
 		plot(1,pch="",xlim=xlim,ylim=ylim,xaxt="n",yaxt="n",xpd=TRUE,ylab=ylab,xlab="",las=1)
-		axis(2,at=yat,labels=TRUE,cex.axis=1,las=1)
+		axis(2,at=yat,labels=TRUE,cex.axis=cex.axis,las=1)	
 	}
-	axis(1,at=1:n,labels=xlab,cex.axis=1)  # add x-axis labels
+	axis(1,at=1:n,labels=xlab,cex.axis=cex.axis,padj=padj)  # add x-axis labels
 	
 	# axis(4,at=seq(-10,10,0.5),labels=TRUE,las=1,cex.axis=cex)	
 	# mtext(expression(hat(beta)[1]),side=4,line=3.1,cex=1,las=1)
